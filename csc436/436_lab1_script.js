4dc4e79f0d1f01b6c3a0ff727be9337e3477f274
Chobii39 <bronch42@gmail.com>
2026-09-15 20:21:39 -0400

    0.9.7.2 reformatted css and minor JS edits

--- csc436/436_lab1_script.js
+++ csc436/436_lab1_script.js
@@ -2,11 +2,17 @@ const cardCell = document.getElementById("cardCell");
 const cards = [
     `565fc4d4-4c17-44d5-a5bc-31bc99018e5d`, // Armageddon
     `4c7c9072-d14c-442c-a386-bfdc0cbe110d`, // Propaganda
-    `test`
+    `906482a9-4b9d-4fc3-a3e5-ac135ed076d3`, // Miku, Child of Song
+    `43c037e3-7d1a-48ca-8ecc-276696592f62`, // Heroic Intervention
+    `f2c4f80e-84a0-463b-82c3-5c6503809351`, // Doubling Season
+    `d6914dba-0d27-4055-ac34-b3ebf5802221`, // Rhystic Study
+    `test` // testing error handling
 ];
 
 async function fetchCards()
 {
+    
+    
     for (let cardID of cards)
     {
         const cardExist = localStorage.getItem(`cachedCard${cardID}`);
@@ -42,14 +48,13 @@ function showCard(cardID)
     if (card.object === 'card')
     {
         const cardHTML = document.createElement("div");
+        cardHTML.classList.add("card");
 
         console.log(card.image_uris.normal);
 
         cardHTML.innerHTML =
         `
-        <div id="cardCell">
-            <img src="${card.image_uris.normal}" alt="${card.name}" class="card-image">
-        </div>
+        <img src="${card.image_uris.normal}" alt="${card.name}" class="card-image">
         `;
         cardCell.appendChild(cardHTML);
     }
